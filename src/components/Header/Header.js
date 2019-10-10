7783d0e41790e7960f4944eea53cbabee285450c
Ditobaskoro <dito.anwar@yahoo.com>
2019-10-11 01:35:22 +0700

    added basic function

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+import Input from '../Input';
+
+
+const Header = props => {
+  return (
+    <div className="header">
+      <div className="header-sub">USD - United States Dollars</div>
+      <div className="header-title">
+        USD
+        <Input {...props} />
+      </div>
+      
+    </div>
+  );
+}
+
+export default Header;
