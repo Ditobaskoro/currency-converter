4548fa22c2e0b02a65e880c4b0e21633b78d3ec8
Ditobaskoro <dito.anwar@yahoo.com>
2019-10-11 13:29:58 +0700

    added function to remove currency

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import './home.css';
+import {
+  Card,
+  Button,
+  Spin,
+  Icon,
+  message,
+} from 'antd';
 import Header from '../Header';
 import rateList from './list.json';
-import { Card, Select, Button, Spin, Icon, message } from 'antd';
-const { Option } = Select;
+import CurrencyOption from '../commons/Select.js';
 
-const CurrencyItem = ({name, rates, value}) => {
-  //target currency list item
+const CurrencyItem = ({ name, rates, value, onRemove }) => {
+  // target currency list item
   const count = rates[name] * (value || 0);
   const parseCount = count.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   const parseRate = rates[name].toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   
   return (
-    <Card 
+    <Card
       title={
         <div className="content-container">
           <div className="content-title">
@@ -25,7 +31,7 @@ const CurrencyItem = ({name, rates, value}) => {
         </div>
       } 
       extra={
-        <a href="#">
+        <a href="/" onClick={e => onRemove(e,name)}>
           <Icon type="close-circle" />
         </a>
       } 
@@ -37,33 +43,16 @@ const CurrencyItem = ({name, rates, value}) => {
   )
 }
 
-const CurrencyOption = ({rateList, selectCurrency}) => {
-  return (
-    <Select
-    showSearch
-    style={{ width: 200 }}
-    placeholder="Select a currency"
-    optionFilterProp="children"
-    onChange={value => selectCurrency(value)}
-    filterOption={(input, option) =>
-      option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-    }
-    >
-      {Object.keys(rateList).map(item => (<Option value={item} key={item}>{item}</Option>))}
-    </Select>
-  )
-}
-
 const Home = () =>  {
-  const [value, setValue] = useState(10.00); //initial default value
-  const [rates, setRates] = useState([]); //rates data from api
-  const [query, setQuery] = useState(['IDR','GBP']); //default target currency
-  const [isLoading, setIsLoading] = useState(false); //loading
-  const [isAdding, addCurrency] = useState(false); //toggle add new target currency
-  const [newCurrency, selectCurrency] = useState(''); //new target currency selection
+  const [value, setValue] = useState(10.00); // initial default value
+  const [rates, setRates] = useState([]); // rates data from api
+  const [query, setQuery] = useState(['IDR','GBP']); // default target currency
+  const [isLoading, setIsLoading] = useState(false); // loading
+  const [isAdding, addCurrency] = useState(false); // toggle add new target currency
+  const [newCurrency, selectCurrency] = useState(''); // new target currency selection
 
   useEffect(() => {
-    //fetch rates api
+    // fetch rates api
     const fetchData = async () => {
       setIsLoading(true);
       const result = await axios(`https://api.exchangeratesapi.io/latest?base=USD&symbols=${query.join(',')}`);
@@ -74,8 +63,8 @@ const Home = () =>  {
   }, [query]);
 
   const onAddCurrency = useCallback(() => {
-    //adding new currency
-    if(query.indexOf(newCurrency) === -1){
+    // adding new currency
+    if (query.indexOf(newCurrency) === -1) {
       setQuery([...query, newCurrency]);
       message.success('Currency added');
     } else {
@@ -84,16 +73,26 @@ const Home = () =>  {
     addCurrency(!isAdding);
   }, [query, newCurrency, isAdding]);
 
+  const onRemoveCurrency = (e, currency) => {
+    e.preventDefault();
+    const newQuery = query.filter(item => item !== currency);
+    setQuery(newQuery);
+    message.success('Currency removed');
+  };
+
   return (
     <div className="home">
       <Header value={value} onChange={value => setValue(value)} />
       <div className="home-content"> 
         {!isLoading ? (
           <div className="content-item">
-            {Object.keys(rates).length === query.length && query.map((item, index)  => (<CurrencyItem name={item} key={index} rates={rates} value={value} />))}
+            {Object.keys(rates).length === query.length
+            && query.map((item) =>
+              (<CurrencyItem name={item} key={item} rates={rates} value={value} onRemove={onRemoveCurrency} />)
+            )}
           </div>
         ) : (
-          <Spin /> 
+          <Spin />
         )}
       </div>
       <div className="home-action">
@@ -101,7 +100,7 @@ const Home = () =>  {
           <div onClick={() => addCurrency(!isAdding)} >Add More Currencies</div>
         ) : (
           <div>
-            <CurrencyOption rateList={rateList} selectCurrency={selectCurrency} />
+            <CurrencyOption list={rateList} onChange={selectCurrency} />
             <Button onClick={onAddCurrency}>Submit</Button>
           </div>
         )}
@@ -110,4 +109,4 @@ const Home = () =>  {
   )
 }
 
-export default Home
+export default Home;
