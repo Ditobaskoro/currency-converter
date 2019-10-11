152f169573d5fb461a323def20f6f369a37c5693
Ditobaskoro <dito.anwar@yahoo.com>
2019-10-11 11:32:10 +0700

    added function for adding  new currency

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import { Input } from 'antd';
 
-const formatNumber = value => {
-  value += '';
-  const list = value.split('.');
-  const prefix = list[0].charAt(0) === '-' ? '-' : '';
-  let num = prefix ? list[0].slice(1) : list[0];
-  let result = '';
-  while (num.length > 3) {
-    result = `,${num.slice(-3)}${result}`;
-    num = num.slice(0, num.length - 3);
-  }
-  if (num) {
-    result = num + result;
-  }
-  return `${prefix}${result}${list[1] ? `.${list[1]}` : ''}`;
-}
-
-
 const InputNumber = props => {
 
   const onInputChange = e => {
+    //allow only number
     const { value } = e.target;
     const reg = /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/;
     if ((!isNaN(value) && reg.test(value)) || value === '' || value === '-') {
@@ -30,12 +14,12 @@ const InputNumber = props => {
 
   return (
     <Input
-      {...props}
-      onChange={onInputChange}
-      autoFocus={true}
-      placeholder="Input a number"
-      prefix="$"
-      style={{ width: '200px', float: 'right', marginRight: '10px' }}
+    {...props}
+    onChange={onInputChange}
+    autoFocus={true}
+    placeholder="Input a number"
+    prefix="$"
+    style={{ width: '200px', float: 'right', marginRight: '10px' }}
     />
   );
 }
