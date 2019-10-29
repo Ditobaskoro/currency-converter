3bd86fb229567d2a56e0bd21756ca44085390e8a
Ditobaskoro <dito.anwar@yahoo.com>
2019-10-29 17:28:43 +0700

    add prettier

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -1,45 +1,43 @@
-
-import { useState, useCallback } from 'react';
-import { message } from 'antd';
+import { useState, useCallback } from 'react'
+import { message } from 'antd'
 
 export default function useCurrency() {
-
   const [initQuery] = useState(() => {
     let initQuery = []
-    if (localStorage.getItem('query')){
+    if (localStorage.getItem('query')) {
       console.log('sss')
-      initQuery = JSON.parse(localStorage.getItem('query'));
+      initQuery = JSON.parse(localStorage.getItem('query'))
     } else {
-      initQuery = ['IDR', 'GBP'];
-      localStorage.setItem('query', JSON.stringify(initQuery));
+      initQuery = ['IDR', 'GBP']
+      localStorage.setItem('query', JSON.stringify(initQuery))
     }
     return initQuery
-  });
+  })
 
-  const [value, setValue] = useState('10');
-  const [query, setQuery] = useState(initQuery);
-  const [isAdding, addCurrency] = useState(false);
-  const [newCurrency, selectCurrency] = useState('');
-  const [base, selectBase] = useState('USD');
+  const [value, setValue] = useState('10')
+  const [query, setQuery] = useState(initQuery)
+  const [isAdding, addCurrency] = useState(false)
+  const [newCurrency, selectCurrency] = useState('')
+  const [base, selectBase] = useState('USD')
 
   const handleAddCurrency = useCallback(() => {
     if (query.indexOf(newCurrency) === -1) {
-      setQuery([...query, newCurrency]);
-      localStorage.setItem('query', JSON.stringify([...query, newCurrency]));
-      message.success('Currency added');
+      setQuery([...query, newCurrency])
+      localStorage.setItem('query', JSON.stringify([...query, newCurrency]))
+      message.success('Currency added')
     } else {
-      message.info('Currency already exist');
+      message.info('Currency already exist')
     }
-    addCurrency(!isAdding);
-  }, [query, newCurrency, isAdding]);
+    addCurrency(!isAdding)
+  }, [query, newCurrency, isAdding])
 
   const handleRemoveCurrency = (e, currency) => {
-    e.preventDefault();
-    const newQuery = query.filter(item => item !== currency);
-    localStorage.setItem("query", JSON.stringify(newQuery));
-    setQuery(newQuery);
-    message.success('Currency removed');
-  };
+    e.preventDefault()
+    const newQuery = query.filter(item => item !== currency)
+    localStorage.setItem('query', JSON.stringify(newQuery))
+    setQuery(newQuery)
+    message.success('Currency removed')
+  }
 
-  return [handleAddCurrency, handleRemoveCurrency, addCurrency, value, setValue, selectCurrency, isAdding, query, base, selectBase];
+  return [handleAddCurrency, handleRemoveCurrency, addCurrency, value, setValue, selectCurrency, isAdding, query, base, selectBase]
 }
